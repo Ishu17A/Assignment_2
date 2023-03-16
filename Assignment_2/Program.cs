d8c6fb1bdb91c402ca07e5223062a8d2459be92f
ishan <ishan@DESKTOP-TOATOCU>
2023-03-16 12:59:05 +0530

    Add project files.

---
+++ Assignment_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Assignment_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Assignment = 2*/
+
+            Console.WriteLine("enter the data :");
+
+            string file = @"D:\file2.txt";
+            FileStream fileStream = new FileStream(file, FileMode.Append, FileAccess.Write);
+            bool flag = true;
+            while (flag)
+            {
+                string data = Console.ReadLine();
+
+                if (data == "stop" || data == "Stop" || data == "")
+                {
+                    flag = false;
+
+
+
+
+
+
+
+                }
+                else
+                {
+                    data += "\n";
+                    byte[] writeArr = Encoding.UTF8.GetBytes(data);
+                    fileStream.Write(writeArr, 0, writeArr.Length);
+                    Console.WriteLine("Data added!");
+                }
+                Console.WriteLine("write stop to quit the program ");
+            }
+            fileStream.Close();
+        }
+    }
+}
